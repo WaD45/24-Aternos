8ce02e8a5835e92c601f38e7e08d5969064a7071
WaDomatic <bad.luck45340@gmail.com>
2022-08-28 11:47:00 +0200

    Update bot.js

--- JS/bot.js
+++ JS/bot.js
@@ -4,8 +4,8 @@ console.log('Starting...')
 
 function createBot () {
     const bot = mineflayer.createBot({
-    host: "localhost",
-    port: "25565",
+    host: "wadomatic.aternos.me",
+    port: "33655",
     username: "Test",
     version: false
     })
